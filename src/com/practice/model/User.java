eb6c294e611dc5f43e88d8a39b65bed180f8fb1c
braj <brajthetdh@gmail.com>
2020-08-16 18:58:23 +0530

    added getter and setter for age field

--- src/com/practice/model/User.java
+++ src/com/practice/model/User.java
@@ -6,6 +6,14 @@ public class User {
 	
 	private String age;
 
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
 	public String getName() {
 		return name;
 	}
