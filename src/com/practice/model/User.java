03bf656c38758040a774cff5a7c9a4c1042377d3
braj <brajthetdh@gmail.com>
2020-08-16 18:53:03 +0530

    added getter and setter for name field

--- src/com/practice/model/User.java
+++ src/com/practice/model/User.java
@@ -3,5 +3,14 @@ package com.practice.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 }
