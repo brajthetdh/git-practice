c753c7ebed551574fe365d01265b0aee2e85e5e9
Braj Bhushan Kumar <45268154+brajthetdh@users.noreply.github.com>
2020-12-07 15:15:27 +0530

    Update User2.java

--- src/com/practice/model/User2.java
+++ src/com/practice/model/User2.java
@@ -6,22 +6,7 @@ public class User {
 	
 	private String age;
 
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+	
 	@Override
 	public String toString() {
 		return "User [name=" + name + ", age=" + age + "]";
